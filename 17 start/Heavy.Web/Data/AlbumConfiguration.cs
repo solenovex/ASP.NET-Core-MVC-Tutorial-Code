7118edda251ec07ac132ee6aac39ce6b7965c271
Dave Young <solenovex@live.com>
2019-01-03 20:42:10 +0800

    updatre

---
+++ 17 start/Heavy.Web/Data/AlbumConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Heavy.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Heavy.Web.Data
+{
+    public class AlbumConfiguration: IEntityTypeConfiguration<Album>
+    {
+        public void Configure(EntityTypeBuilder<Album> builder)
+        {
+            builder.Property(x => x.Title).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Artist).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.ReleaseDate).HasColumnType("date");
+            builder.Property(x => x.Price).HasColumnType("decimal(6,2)");
+            builder.Property(x => x.CoverUrl).IsRequired().HasMaxLength(200);
+        }
+    }
+}
---
+++ 17 start/Heavy.Web/Models/Album.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Heavy.Web.Models
+{
+    public class Album
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "专辑名")]
+        public string Title { get; set; }
+
+        [Display(Name = "艺人")]
+        public string Artist { get; set; }
+
+        [Display(Name = "发行日期")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Display(Name = "价格")]
+        public decimal Price { get; set; }
+
+        [Display(Name = "封面地址")]
+        public string CoverUrl { get; set; }
+    }
+}
+
