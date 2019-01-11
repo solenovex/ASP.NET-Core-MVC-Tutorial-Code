a19403ac6f2deecaa5c739d9c1f65b9937a76665
Dave Young <solenovex@live.com>
2019-01-11 20:06:28 +0800

    update

---
+++ 23 start/Heavy.Web/ViewModels/UserEditViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Heavy.Web.ViewModels
+{
+    public class UserEditViewModel
+    {
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "用户名")]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])", ErrorMessage = "Email的格式不正确")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Display(Name = "身份证号")]
+        [StringLength(18, MinimumLength = 18, ErrorMessage = "{0}的长度是{1}")]
+        public string IdCardNo { get; set; }
+
+        [Required]
+        [Display(Name = "出生日期")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        public List<string> Claims { get; set; }
+    }
+}
